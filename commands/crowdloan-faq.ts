c770641ca8bba9405b85a54b28e29b3d18a2ef1e
j7kessler <72840363+j7kessler@users.noreply.github.com>
2021-11-03 21:02:30 -0400

    Update crowdloan-faq.ts

--- commands/crowdloan-faq.ts
+++ commands/crowdloan-faq.ts
@@ -1,19 +1,34 @@
 const crowdloanFAQ = `
 # Acala Crowdloan FAQ
 
-## rewards-same
-### Is the reward ratio the same across contribution methods?
-Yes. The base ACA reward is the same regardless of how you participate.
+## get-referral
+### How do I get a referral code?
+There are two ways to get a referral code. Users on the waitlist can complete the first two Acala Quests to receive their referral code via email. Users not on the waitlist can receive a referral code after they make their crowdloan contribution.
 
 
-## number-aca
-### How many ACA will I receive for each DOT contributed?
-The ACA to DOT ratio will be announced at a later time.
+## change-referral
+### How do I change the DOT address linked to my referral code?
+Users on the waitlist are the only users that can change the DOT address linked to their referral code.
+To change the DOT address, redo Quests 1 and 2 using your waitlist email address and the new DOT address you'd like to use for your referral link. Your new referral link will be emailed to you.
+
+## 10%-bonus
+### How do I get my 10% referral bonus?
+To get your 10% ACA bonus, enter YOUR referral code to self-refer when you make YOUR crowdloan contribution. You'll receive a 5% ACA bonus as the referrer and 5% ACA bonus as referee for a total of 10% bonus on YOUR DOT contribution. Share YOUR referral code with others and receive a 5% bonus in ACA on THEIR DOT contributions.
 
 
-## minimum
-### What is the minimum amount I can contribute?
-The minimum contribution depends on how you participate. The minimum for Liquid Crowdloan Contribution is 1 DOT. The minimum for Direct Contribution and Partner Wallets is 5 DOT. Check with Exchanges to see their minimum requirements. 
+## 5%-bonus
+### How do I get my 5% referral bonus?
+Enter SOMEONE ELSE'S referral code when you make YOUR crowdloan contribution to receive a 5% bonus in ACA on YOUR DOT contribution. After you make YOUR crowdloan contribution, Acala will give you YOUR OWN referral code that you can share with others to receive 5% bonus in ACA on THEIR DOT contributions.
+
+
+## 2%-karura
+### How do I get my 2% Karura Crowdloan bonus?
+If you contributed to the Karura Crowdloan, you must make your Acala Crowdloan contribution with the same account that you used to make your Karura contribution. If you do this, you will receive a 2% bonus in ACA on YOUR DOT contribution.
+
+
+## bonus-chart
+### Can you show how the bonuses can be used together?
+This is a simplified chart of how the bonuses can be used together https://wiki.acala.network/acala/acala-crowdloan/faq#can-you-show-how-the-bonuses-can-be-used-together. Please read the FAQ to understand the specific requirements for each bonus and how bonus rewards are calculated.
 
 
 ## how-lcdot
@@ -36,9 +51,28 @@ Liquid Crowdloan DOT (lcDOT) represents the underlying DOT liquidity of an Acala
 Contributors that made a Liquid Crowdloan Contribution must hold lcDOT at the end of the parachain lease in order to redeem their lcDOT for DOT. If these contributors no longer hold lcDOT, they won’t be able to redeem for DOT. However, selling lcDOT has no impact on ACA rewards. 
 
 
+## price-lcdot
+### What will be the price of lcDOT?
+The price of lcDOT will be determined by the free market. Acala does not control the price of lcDOT. We also have a policy of not discussing price in this server. You can discuss price in community-run, Acala+ server https://discord.gg/HCpBgJc7. 
+
+
+## price-talk
+We do not discuss price in this server. You can discuss price in community-run, Acala+ server https://discord.gg/HCpBgJc7.
+
+
+## number-aca
+### How many ACA will I receive for each DOT contributed?
+The ACA to DOT ratio will be announced at a later time. 170,000,000 ACA tokens (17% of the fixed supply) will be distributed to crowdloan contributors.
+
+
 ## wen-aca
 ### When will I receive my ACA tokens?
-You will receive your ACA after Acala wins a parachain slot, launches the network, and enables token transfers.
+You will receive your ACA after Acala wins a parachain slot, launches the network (expected to be December 15, 2021), and enables token transfers.
+
+
+## vesting
+### What is the vesting schedule?
+20% of your ACA rewards will be delivered to you as liquid and transferrable. The remaining 80% will vest/unlock linearly (every block) over the 96 week parachain lease.
 
 
 ## faq
@@ -54,6 +88,7 @@ You can find more information by visiting https://wiki.acala.network/acala/acala
 ## ledger
 ### Can I use Ledger?
 No. Do not use Ledger. Ledger does not support Acala and you won't be able to access your ACA tokens. We can not and will not help you get your ACA tokens if you use Ledger.
+
 
 ## quests-help
 ### How can I learn more about Acala Quests?
