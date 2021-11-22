1e06601b7ff841691aebd02531993a24b1dcd253
j7kessler <72840363+j7kessler@users.noreply.github.com>
2021-11-22 17:14:58 -0500

    Update crowdloan-faq.ts

--- commands/crowdloan-faq.ts
+++ commands/crowdloan-faq.ts
@@ -1,60 +1,9 @@
 const crowdloanFAQ = `
 # Acala Crowdloan FAQ
 
-## get-referral
-### How do I get a referral code?
-There are two ways to get a referral code. Users on the waitlist (needed to sign up by Oct 12) can complete the first two Acala Quests to receive their referral code via email. Users not on the waitlist can receive a referral code after they make their crowdloan contribution.
-
-
-## change-referral
-### How do I change the DOT address linked to my referral code?
-Users on the waitlist are the only users that can change the DOT address linked to their referral code.
-To change the DOT address, redo Quests 1 and 2 using your waitlist email address and the new DOT address you'd like to use for your referral link. Your new referral link will be emailed to you.
-
-
-## 10-bonus
-### How do I get my 10% referral bonus?
-To get your 10% ACA bonus, enter YOUR referral code to self-refer when you make YOUR crowdloan contribution. You'll receive a 5% ACA bonus as the referrer and 5% ACA bonus as referee for a total of 10% bonus on YOUR DOT contribution. Share YOUR referral code with others and receive a 5% bonus in ACA on THEIR DOT contributions.
-
-
-## 5-bonus
-### How do I get my 5% referral bonus?
-Enter SOMEONE ELSE'S referral code when you make YOUR crowdloan contribution to receive a 5% bonus in ACA on YOUR DOT contribution. After you make YOUR crowdloan contribution, Acala will give you YOUR OWN referral code that you can share with others to receive 5% bonus in ACA on THEIR DOT contributions.
-
-
-## 2-karura
-### How do I get my 2% Karura Crowdloan bonus?
-If you contributed to the Karura Crowdloan, you must make your Acala Crowdloan contribution with the same account that you used to make your Karura contribution. If you do this, you will receive a 2% bonus in ACA on YOUR FIRST DOT contribution.
-
-
-## bonus-chart
-### Can you show how the bonuses can be used together?
-This is a simplified chart of how the bonuses can be used together https://wiki.acala.network/acala/acala-crowdloan/faq#can-you-show-how-the-bonuses-can-be-used-together. Please read the FAQ to understand the specific requirements for each bonus and how bonus rewards are calculated.
-
-
 ## check-contribution
 ### How do I check my crowdloan contribution?
 Go to https://acala.network/acala/join-acala/bonus to check your contribution and track your rewards.
-
-
-## reaped
-### I made a contribution but it seems like I lost an extra ~1 DOT - what happened?
-Polkadot requires that all non-zero balances be 1 DOT or greater. If the account balance falls below this amount, the 0.X DOT are automatically taken from the user by the Polkadot platform and cannot be returned. Because of this, it's important that users leave at least 1.1 DOT balance after they make their crowdloan contribution.
-
-
-## how-lcdot
-### How does Liquid Crowdloan Contribution work? 
-Select the Liquid Crowdloan option when making your contribution on the Acala website or Polkawallet. After the Acala network launches, you will receive your ACA rewards plus lcDOT. lcDOT will be issued on a 1:1 basis against your DOT contribution and can be redeemed for DOT on a 1:1 basis at Acala's parachain lease expiration. lcDOT allows you to access the liquidity in your crowdloan contribution because lcDOT can be used in Acala’s DeFi primitives (e.g., trade, transfer, collateralize). It's important to note that the Liquid Crowdloan option is not completely trustless. It requires you to contribute your DOT to the Liquid Crowdoan Vault managed by the Acala Foundation. This vault will be managed by the Acala Foundation until ownership of the vault is transferred to the Acala parachain. If you are looking for a way to trustlessly contribute, make a Direct Polkadot Contribution.
-
-
-## risks-lcdot
-### What are the risks of the Liquid Crowdloan Contribution method?
-Contributors using this method need to trust the Acala Foundation to manage the Liquid Crowdloan Vault until Vault ownership is transferred to the Acala parachain. We do not foresee any technical risk with this approach.
-
-
-## what-lcdot
-### What is lcDOT?
-Liquid Crowdloan DOT (lcDOT) represents the underlying DOT liquidity of an Acala Crowdloan DOT contribution. When a participant contributes DOT via the Liquid Crowdloan option, they will receive lcDOT, a liquid (unlocked) token available for use on Acala’s DeFi hub in addition to ACA rewards. lcDOT can be used to swap, collateralize for a self-serviced loan in aUSD stablecoin, and more. lcDOT is redeemable for DOT on a 1:1 basis at the end of Acala’s parachain lease.
 
 
 ## sell-lcdot
@@ -72,34 +21,35 @@ The price of lcDOT will be determined by the free market. Acala does not control
 We do not discuss price in this server. You can discuss price in community-run, Acala+ server https://discord.gg/HCpBgJc7.
 
 
-## number-aca
-### How many ACA will I receive for each DOT contributed?
-170,000,000 ACA tokens (17% of the fixed supply) will be distributed to crowdloan contributors. The final amount of ACA you receive is dependent on the total amount of DOT contributed to the Acala crowdloan.
-
-
-## wen-aca
+## wen-tokens
 ### When will I receive my ACA tokens?
-You will receive your ACA after Acala wins a parachain slot, launches the network (expected to be December 15, 2021), and enables token transfers.
+We know that the community is eager to receive their tokens. The distribution of tokens is one of the launch phases in our rollout. The Acala network is expected to launch on December 17th and tokens from Build Acala and the crowdloan will be distributed after transfers are enabled.
 
 
-## vesting
+## claim-tokens
+### Do I need to claim my tokens?
+Only users that contributed to the crowdloan directly through the Polkadot.js.org WEBSITE need to go through the claims process. The reason they need to do this is because they have not yet agreed to the Terms and Conditions of the crowdloan. We'll soon be publishing instructions on how users that need to go through claims can do this.
+
+
+## where-tokens
+### Where will I receive my tokens?
+Crowdloan contributors will receive their tokens in the account they used to make their crowdloan contribution. There is no need to set up a new account or wallet. Build Acala participants will receive their tokens in the DOT account they listed in their profile on the Build Acala event platform.
+
+
+## reap-rewards
+### Will I still receive my tokens if my account fell below the existential deposit?
+Yes. This will not impact your ability to receive rewards.
+
+
+## build-vesting
 ### What is the vesting schedule?
-20% of your ACA rewards will be delivered to you as liquid and transferrable. The remaining 80% will vest/unlock linearly (every block) over the 96 week parachain lease.
+Participants in the April Build Acala event have no vesting on their ACA tokens. Participants in the August/September Build Acala event have a 3 month vest on their ACA tokens.
 
 
-## faq
-### Where can I find the answer to my question?
-You can find the answer to your qeustion by visiting https://wiki.acala.network/acala/acala-crowdloan/faq
+## crowdloan-vesting
+### What is the vesting schedule?
+20% of your ACA crowdloan rewards will be delivered to you as liquid and transferrable. The remaining 80% will vest/unlock linearly (every block) over the 96 week parachain lease.
 
-
-## how-participate
-### Where can I learn more about how to participate?
-You can find more information by visiting https://wiki.acala.network/acala/acala-crowdloan/crowdloan-event
-
-
-## ledger
-### Can I use Ledger?
-No. Do not use Ledger. Ledger does not support Acala and you won't be able to access your ACA tokens. We can not and will not help you get your ACA tokens if you use Ledger.
 
 `;
 
